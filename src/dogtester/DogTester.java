4cd67f26b43c7e77d89986a017d66c01aa9ce67a
rkaune <rkaune@ugcloud.ca>
2022-04-11 13:21:29 -0400

    Initial commit

--- src/dogtester/DogTester.java
+++ src/dogtester/DogTester.java
@@ -6,14 +6,15 @@ public class DogTester {
 
     public static void main(String[] args) {
         Dog dog1 = new Dog("Rover", "Spaniel", 8, 4);
-        Dog dog2 = new Dog("Spot", "Border Collie");
-        Dog dog3 = new Dog();
+        Dog dog2 = new Dog();
+        Dog dog3 = dog2;
         
         //dog1.name = "spot";
         dog1.setAggression(1);
         System.out.println(dog1.getAggression());
         System.out.println(dog2.getAggression());
         System.out.println(dog3.getAggression());
+        System.out.println(dog2.equals(dog3));
 
         //Print the details of each dog
         System.out.println(dog1.toString());
